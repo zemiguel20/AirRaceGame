17a4bce11385a67f2bbc2d135bc9f2a2ccb7065b
zemiguel20 <migli.coutinho@gmail.com>
2021-04-15 22:30:50 +0100

    #2 Input handling for player added

--- AirRace_Project/Assets/Scripts/Player/PlayerController.cs
+++ AirRace_Project/Assets/Scripts/Player/PlayerController.cs
@@ -1,20 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody plane;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 inputVector;
+    private bool moving;
+
+    private void Start()
     {
+        inputVector = Vector2.zero;
+        moving = false;
+    }
+
+    private void FixedUpdate()
+    {
+        plane.velocity = Vector3.zero; //Reset velocity
+
+        // apply plane turning movement modifiers
+        if (moving)
+        {
+            Debug.Log("Turning to be defined");
+        }
+
+        // apply forward velocity
+        plane.AddRelativeForce(Vector3.forward * 10, ForceMode.VelocityChange);
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        inputVector = context.ReadValue<Vector2>();
+
+        if (inputVector.Equals(Vector2.zero))
+            moving = false;
+        else
+            moving = true;
     }
 }
