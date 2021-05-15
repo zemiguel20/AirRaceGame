c642559468ad4456a69cd29bc9c18353ddd168f6
zemiguel20 <migli.coutinho@gmail.com>
2021-05-15 19:09:09 +0100

    Goal prefab refactored
    Goal prefab now has collider and script on root object, mesh used in collider is now a imported circle mesh. Name of script changed.

--- AirRace_Project/Assets/Scripts/Race/Goal.cs
+++ AirRace_Project/Assets/Scripts/Race/Goal.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoalCollision : MonoBehaviour
+public class Goal : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
