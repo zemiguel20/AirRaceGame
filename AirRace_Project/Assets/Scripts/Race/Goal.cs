c7c3aeeb0ccea21217792ca8346dd699b2448dd5
zemiguel20 <migli.coutinho@gmail.com>
2021-05-15 22:57:14 +0100

    #6 Goal callback implemented
    implemented callback to RaceManager

--- AirRace_Project/Assets/Scripts/Race/Goal.cs
+++ AirRace_Project/Assets/Scripts/Race/Goal.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+
+    public RaceManager raceManager;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "GoalHitter")
-            // TODO - implement callback
-            Debug.LogError("Goal hit callback not implemented!");
+            raceManager?.OnGoalHit();
     }
 }
