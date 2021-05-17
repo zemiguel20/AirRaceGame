fa85974309bd40aa6fa3bb6d58edeac97dfb185e
zemiguel20 <migli.coutinho@gmail.com>
2021-05-17 23:15:40 +0100

    #6 RaceManager script implemented

--- AirRace_Project/Assets/Scripts/Race/RaceManager.cs
+++ AirRace_Project/Assets/Scripts/Race/RaceManager.cs
@@ -6,8 +6,97 @@ using UnityEngine.Events;
 
 public class RaceManager : MonoBehaviour
 {
+    public List<Goal> goals;
+    public float TIME_LIMIT;
+
+    private bool raceStarted;
+    private int goalsPassed;
+    private float timeCounter;
+    private float score;
+
+    private void Start()
+    {
+        raceStarted = false;
+
+        //Will be replaced by GameManager
+        StartRace();
+    }
+
+    private void Update()
+    {
+        if (raceStarted)
+        {
+            timeCounter += Time.deltaTime;
+        }
+    }
+
     public void OnGoalHit()
     {
-        Debug.LogWarning("OnGoalHit not implemented");
+        // Will be replaced with GUI
+        Debug.Log("Time: " + timeCounter);
+
+        score += CalculatePoints(timeCounter);
+        timeCounter = 0;
+        goalsPassed++;
+        Goal passedGoal = goals[goalsPassed - 1];
+        passedGoal.gameObject.SetActive(false);
+
+        if (goalsPassed >= goals.Count)
+        {
+            raceStarted = false;
+
+            // Will be replaced with GUI
+            Debug.Log("Finished with: " + score);
+        }
+        else
+        {
+            Goal nextGoal = goals[goalsPassed];
+            nextGoal.gameObject.SetActive(true);
+        }
+    }
+
+    private float CalculatePoints(float playerTime)
+    {
+        float points = 0;
+
+        if (playerTime < TIME_LIMIT)
+            points = TIME_LIMIT - playerTime;
+
+
+        points = (float)Math.Round(points, 1) * 10;
+
+        return points;
+    }
+
+    public void StartRace()
+    {
+        if (raceStarted)
+        {
+            Debug.Log("Race already started");
+            return;
+        }
+
+        if(goals.Count <= 0)
+        {
+            Debug.LogWarning("Cant start a race with no goals!");
+            return;
+        }
+
+
+        foreach (Goal goal in goals)
+        {
+            goal.SetRaceManager(this);
+        }
+
+        raceStarted = true;
+        goalsPassed = 0;
+        timeCounter = 0;
+        score = 0;
+
+        //Turns off goals except the first one
+        for (int i = 1; i < goals.Count; i++)
+        {
+            goals[i].gameObject.SetActive(false);
+        }
     }
 }
