79267dcc4db5b64e286b4243a2712235c9e84d19
zemiguel20 <migli.coutinho@gmail.com>
2021-05-17 15:28:15 +0100

    #6 Goal - manager set with function
    RaceManager is now private and set using a function instead of editor

--- AirRace_Project/Assets/Scripts/Race/Goal.cs
+++ AirRace_Project/Assets/Scripts/Race/Goal.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
 
-    public RaceManager raceManager;
+    private RaceManager raceManager;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "GoalHitter")
+        {
             raceManager?.OnGoalHit();
+
+            if (raceManager == null)
+                Debug.LogWarning("No RaceManager attached.");
+        }
+
+    }
+
+    public void SetRaceManager(RaceManager raceManager)
+    {
+        this.raceManager = raceManager;
     }
 }
