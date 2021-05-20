77ad320cbc02e15891ff9b44b7e64b821ca31607
zemiguel20 <migli.coutinho@gmail.com>
2021-05-20 01:07:48 +0100

    CountdownTimerUI now deactivates itself on Start

--- AirRace_Project/Assets/Scripts/UI/CountdownTimerUI.cs
+++ AirRace_Project/Assets/Scripts/UI/CountdownTimerUI.cs
@@ -5,6 +5,11 @@ public class CountdownTimerUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI tmpText;
 
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
     public void SetText(string newText)
     {
         tmpText.text = newText;
