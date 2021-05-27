753b481bddfa7628b6fa5cb3270c7e627dc4a195
zemiguel20 <migli.coutinho@gmail.com>
2021-05-27 19:05:21 +0100

    Created a Disable On Start script

--- AirRace_Project/Assets/Scripts/UI/CountdownTimerUI.cs
+++ AirRace_Project/Assets/Scripts/UI/CountdownTimerUI.cs
@@ -5,11 +5,6 @@ public class CountdownTimerUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI tmpText;
 
-    private void Start()
-    {
-        gameObject.SetActive(false);
-    }
-
     public void SetText(string newText)
     {
         tmpText.text = newText;
--- AirRace_Project/Assets/Scripts/UI/EndGamePanel.cs
+++ AirRace_Project/Assets/Scripts/UI/EndGamePanel.cs
@@ -1,16 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class EndGamePanel : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
-
-    void Start()
-    {
-        gameObject.SetActive(false);
-    }
 
     public void SetInfo(int score)
     {
