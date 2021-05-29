68b1b14e11532c9471104e9b11002bc1c9276b34
zemiguel20 <migli.coutinho@gmail.com>
2021-05-29 23:21:38 +0100

    #22 Added FloatVariable SO

---
+++ AirRace_Project/Assets/Scripts/SOVariables/FloatVariable.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Variables/FloatVariable")]
+public class FloatVariable : ScriptableObject
+{
+    public string description;
+    public float value;
+}
