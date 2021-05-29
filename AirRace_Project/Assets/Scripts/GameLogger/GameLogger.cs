6cbaf7af8e2ab1661aa7f99fa8fe90ed3595efa1
zemiguel20 <migli.coutinho@gmail.com>
2021-05-29 13:38:05 +0100

    Added GameLogger debug wrapper

---
+++ AirRace_Project/Assets/Scripts/GameLogger/GameLogger.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * As recommended in the documentation, development Debug logs should be wrapped in a conditional method
+ * https://docs.unity3d.com/Manual/BestPracticeUnderstandingPerformanceInUnity7.html
+ */
+
+using System.Diagnostics;
+
+namespace Assets.Scripts.GameLogger
+{
+    public static class GameLogger
+    {
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Debug(string logMsg)
+        {
+
+            UnityEngine.Debug.Log(logMsg);
+
+        }
+
+    }
+}
