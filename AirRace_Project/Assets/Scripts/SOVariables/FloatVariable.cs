dc8496a0a56293648c849ff582f44826ec36ce5d
zemiguel20 <migli.coutinho@gmail.com>
2021-05-30 17:22:46 +0100

    FloatVariable - added conditional for description

--- AirRace_Project/Assets/Scripts/SOVariables/FloatVariable.cs
+++ AirRace_Project/Assets/Scripts/SOVariables/FloatVariable.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Variables/FloatVariable")]
 public class FloatVariable : ScriptableObject
 {
+#if UNITY_EDITOR
     [TextArea] public string description;
+#endif
     public float value;
 }
