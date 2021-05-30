f218a2fff003383117a233967dbb22c2f074cc10
zemiguel20 <migli.coutinho@gmail.com>
2021-05-30 02:41:58 +0100

    Added TextArea tag to FloatVariable

--- AirRace_Project/Assets/Scripts/SOVariables/FloatVariable.cs
+++ AirRace_Project/Assets/Scripts/SOVariables/FloatVariable.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Variables/FloatVariable")]
 public class FloatVariable : ScriptableObject
 {
-    public string description;
+    [TextArea] public string description;
     public float value;
 }
