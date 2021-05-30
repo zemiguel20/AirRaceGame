bd01dabae6f8fef34bba1dc78d75a5eb985c7917
zemiguel20 <migli.coutinho@gmail.com>
2021-05-30 01:07:49 +0100

    #22 Added PathManager

---
+++ AirRace_Project/Assets/Scripts/Race/PathManager.cs
@@ -0,0 +1,73 @@
+using Assets.Scripts.GameLogger;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PathManager : MonoBehaviour
+{
+    [SerializeField] private List<Goal> goals;
+    [SerializeField] private UnityEvent PathFinished;
+
+    private List<Goal> goalsPassed = new List<Goal>();
+
+    public void StartPath()
+    {
+        GameLogger.Debug("Path Started");
+
+        if (goals.Count == 0)
+        {
+            EndPath();
+            return;
+        }
+        else
+        {
+            //Turns off goals
+            foreach (Goal goal in goals)
+            {
+                goal.gameObject.SetActive(false);
+            }
+
+            ActivateNextGoal();
+        }
+    }
+
+    public void ChangeActiveGoal()
+    {
+
+
+        if (goals.Count > 0)
+        {
+            RemoveCurrentGoal();
+            GameLogger.Debug("Goal passed! Num of goals passed: " + goalsPassed.Count);
+        }
+
+        if (goals.Count == 0)
+        {
+            EndPath();
+        }
+        else
+        {
+            ActivateNextGoal();
+        }
+    }
+
+    private void ActivateNextGoal()
+    {
+        goals[0].gameObject.SetActive(true);
+    }
+
+    private void RemoveCurrentGoal()
+    {
+        Goal goal = goals[0];
+        goal.gameObject.SetActive(false);
+        goals.Remove(goal);
+        goalsPassed.Add(goal);
+    }
+
+    private void EndPath()
+    {
+        GameLogger.Debug("Path Finished");
+        PathFinished.Invoke();
+    }
+
+}
