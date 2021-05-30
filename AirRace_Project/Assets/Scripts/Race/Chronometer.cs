0a18a9b736e76d4445b17d5a2d7d2d5f3f27de86
zemiguel20 <migli.coutinho@gmail.com>
2021-05-30 02:01:02 +0100

    #22 Chronometer added

---
+++ AirRace_Project/Assets/Scripts/Race/Chronometer.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.GameLogger;
+using UnityEngine;
+
+public class Chronometer : MonoBehaviour
+{
+    [SerializeField] private FloatVariable timeVariable;
+
+    private bool active = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            timeVariable.value += Time.deltaTime;
+        }
+    }
+
+    public void ResetTime()
+    {
+        GameLogger.Debug("Chronometer Reset");
+        timeVariable.value = 0;
+    }
+
+    public void StartChrono()
+    {
+        GameLogger.Debug("Chronometer started");
+        active = true;
+    }
+
+    public void StopChrono()
+    {
+        GameLogger.Debug("Chronometer stopped");
+        active = false;
+    }
+
+}
