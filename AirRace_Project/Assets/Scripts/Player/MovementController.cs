93c7d26823b1ee4e68eb0812b44bd6874bf986ad
zemiguel20 <migli.coutinho@gmail.com>
2021-06-03 15:33:14 +0100

    #22 MovementController - added OnRaceStart and OnRaceEnd

--- AirRace_Project/Assets/Scripts/Player/MovementController.cs
+++ AirRace_Project/Assets/Scripts/Player/MovementController.cs
@@ -1,38 +1,56 @@
+using AirRace.Core.Events;
 using UnityEngine;
 
 namespace AirRace.Player
 {
     public class MovementController : MonoBehaviour
     {
-        private Rigidbody plane;
+        [SerializeField] private Rigidbody _plane;
+        [SerializeField] private EventManager _eventManager;
 
         [Tooltip("Value for max acceleration. Acceleration is given by Input * MaxAcceleration")]
-        public int MAX_ACCELERATION;
+        [SerializeField] private int MAX_ACCELERATION;
 
         [Tooltip("Base rotation speed strength")]
-        public float ROTATION_SPEED;
+        [SerializeField] private float ROTATION_SPEED;
 
         [Tooltip("Velocity value of the plane at which rotation speed is MAX")]
-        public float ROTATION_SPEED_VELOCITY_THRESHOLD;
+        [SerializeField] private float ROTATION_SPEED_VELOCITY_THRESHOLD;
 
         [Tooltip("Value of the velocity of the plane at which the lift is max")]
-        public float LIFT_VELOCITY_THRESHOLD;
+        [SerializeField] private float LIFT_VELOCITY_THRESHOLD;
 
         private float inputAcceleretion;
         private float inputAilerons;
         private float inputElevators;
-
-        private void Awake()
-        {
-            plane = GetComponent<Rigidbody>();
-        }
 
         private void Start()
         {
             inputAcceleretion = 0;
             inputAilerons = 0;
             inputElevators = 0;
+
+            _eventManager.RaceStarted += OnRaceStart;
+            _eventManager.RaceEnded += OnRaceEnd;
         }
+
+        private void OnDisable()
+        {
+            _eventManager.RaceStarted -= OnRaceStart;
+            _eventManager.RaceEnded -= OnRaceEnd;
+        }
+
+        public void OnRaceStart()
+        {
+            _plane.isKinematic = false;
+        }
+
+        public void OnRaceEnd(float time)
+        {
+            _plane.isKinematic = true;
+        }
+
+        // MOVEMENT ----------------------------------------------------------------------------
 
         private void FixedUpdate()
         {
@@ -46,31 +64,31 @@ namespace AirRace.Player
 
             Vector3 direction = new Vector3(inputElevators, 0, -inputAilerons);
 
-            float velocityFactor = Mathf.Clamp01(plane.velocity.magnitude / ROTATION_SPEED_VELOCITY_THRESHOLD);
+            float velocityFactor = Mathf.Clamp01(_plane.velocity.magnitude / ROTATION_SPEED_VELOCITY_THRESHOLD);
 
             Vector3 force = direction * velocityFactor * ROTATION_SPEED;
 
-            plane.AddRelativeTorque(force, ForceMode.Acceleration);
+            _plane.AddRelativeTorque(force, ForceMode.Acceleration);
         }
 
         private void UpdateThrust()
         {
             Vector3 force = Vector3.forward * (inputAcceleretion * MAX_ACCELERATION);
 
-            plane.AddRelativeForce(force, ForceMode.Acceleration);
+            _plane.AddRelativeForce(force, ForceMode.Acceleration);
         }
 
         private void UpdateLift()
         {
             Vector3 baseForce = Physics.gravity * -1;
 
-            float velocityFactor = Mathf.Clamp01(plane.velocity.magnitude / LIFT_VELOCITY_THRESHOLD);
+            float velocityFactor = Mathf.Clamp01(_plane.velocity.magnitude / LIFT_VELOCITY_THRESHOLD);
 
             float inclinationFactor = CalculateInclinationFactor();
 
             Vector3 force = baseForce * velocityFactor * inclinationFactor;
 
-            plane.AddForce(force, ForceMode.Acceleration);
+            _plane.AddForce(force, ForceMode.Acceleration);
         }
 
         private float CalculateInclinationFactor()
@@ -80,7 +98,7 @@ namespace AirRace.Player
             if (isRotating)
                 return 1;
 
-            float angle = plane.transform.localEulerAngles.z;
+            float angle = _plane.transform.localEulerAngles.z;
             // y = 0.0000205761 * (x-180)^2 + 0.333333
             float factor = 0.0000205761f * Mathf.Pow(angle - 180, 2) + 0.333333f;
 
