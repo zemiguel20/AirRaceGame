9597df727eec989329f76bb94463b911c933aac2
zemiguel20 <migli.coutinho@gmail.com>
2021-06-04 16:48:48 +0100

    #12 Leaderboard Scriptable object added

---
+++ AirRace_Project/Assets/Scripts/Core/LeaderboardSerializable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AirRace.Core
+{
+    [System.Serializable]
+    public class LeaderboardSerializable
+    {
+        public int size;
+        public float[] times;
+
+        public LeaderboardSerializable(int size, List<float> times)
+        {
+            this.size = size;
+            this.times = times.ToArray();
+        }
+    }
+}
---
+++ AirRace_Project/Assets/Scripts/Core/SOs/LeaderboardSO.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AirRace.Core.SOs
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Leaderboard")]
+    public class LeaderboardSO : ScriptableObject
+    {
+        [SerializeField]
+        private int LEADERBOARD_SIZE = 10;
+
+        [SerializeField]
+        private List<float> _leaderboard = new List<float>();
+
+        public void AddEntry(float time)
+        {
+            _leaderboard.Add(time);
+
+            _leaderboard.Sort(); //default is ascending order
+
+            // Trims to SIZE
+            if (_leaderboard.Count > LEADERBOARD_SIZE)
+            {
+                _leaderboard = _leaderboard.GetRange(0, LEADERBOARD_SIZE);
+            }
+        }
+
+        public List<float> Values()
+        {
+            return new List<float>(_leaderboard);
+        }
+
+        public int Size()
+        {
+            return LEADERBOARD_SIZE;
+        }
+
+        public LeaderboardSerializable ToSerializable()
+        {
+            return new LeaderboardSerializable(LEADERBOARD_SIZE, _leaderboard);
+        }
+    }
+}
