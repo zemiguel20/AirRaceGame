8400670458993da2e5bea0e8c8975f890aed462f
zemiguel20 <migli.coutinho@gmail.com>
2021-06-05 01:36:03 +0100

    #12 Added Leaderboard part to UI

--- AirRace_Project/Assets/Scripts/UI/Race/EndGamePanel.cs
+++ AirRace_Project/Assets/Scripts/UI/Race/EndGamePanel.cs
@@ -1,3 +1,5 @@
+using AirRace.Core.SOs;
+using System.Text;
 using TMPro;
 using UnityEngine;
 
@@ -5,11 +7,32 @@ namespace AirRace.UI.Race
 {
     public class EndGamePanel : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private TextMeshProUGUI _timeText;
+        [SerializeField] private TextMeshProUGUI _leaderboardText;
+        [SerializeField] private LeaderboardSO _leaderboard;
 
         public void SetInfo(float playerTime)
         {
-            scoreText.text = playerTime.ToString("F2");
+            _timeText.text = playerTime.ToString("F2");
+
+            StringBuilder sb = new StringBuilder();
+
+
+            for (int i = 0; i < _leaderboard.Size(); i++)
+            {
+                try
+                {
+                    float value = _leaderboard.Values()[i];
+                    sb.AppendFormat("{0,-5} {1,-10} {2} \n", (i + 1).ToString(), ".........", value.ToString("F2"));
+                }
+                catch (System.Exception)
+                {
+                    sb.AppendFormat("{0,-5} {1,-10} {2} \n", (i + 1).ToString(), ".........", "N/A");
+                }
+            }
+
+            _leaderboardText.text = sb.ToString();
+
         }
     }
 }
