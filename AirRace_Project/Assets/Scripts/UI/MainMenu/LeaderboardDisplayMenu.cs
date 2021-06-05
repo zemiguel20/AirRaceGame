8cb3c1fec01ab7f4ed2f8130d0ef50980e877a02
zemiguel20 <migli.coutinho@gmail.com>
2021-06-05 02:27:44 +0100

    #21 Added menu pages and Leaderboard loading and info

---
+++ AirRace_Project/Assets/Scripts/UI/MainMenu/LeaderboardDisplayMenu.cs
@@ -0,0 +1,35 @@
+﻿using AirRace.Core.SOs;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace AirRace.UI.MainMenu
+{
+    public class LeaderboardDisplayMenu : MonoBehaviour
+    {
+
+        [SerializeField] private TextMeshProUGUI _leaderboardText;
+        [SerializeField] private LeaderboardSO _leaderboard;
+
+        private void OnEnable()
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            for (int i = 0; i < _leaderboard.Size(); i++)
+            {
+                try
+                {
+                    float value = _leaderboard.Values()[i];
+                    sb.AppendFormat("{0,-5} {1,-10} {2} \n", (i + 1).ToString(), ".........", value.ToString("F2"));
+                }
+                catch (System.Exception)
+                {
+                    sb.AppendFormat("{0,-5} {1,-10} {2} \n", (i + 1).ToString(), ".........", "N/A");
+                }
+            }
+
+            _leaderboardText.text = sb.ToString();
+        }
+    }
+}
---
+++ AirRace_Project/Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -0,0 +1,39 @@
+using AirRace.Core;
+using AirRace.Core.SOs;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AirRace.UI.MainMenu
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        [SerializeField] private List<LeaderboardSO> _leaderboards;
+
+        private void Awake()
+        {
+            BootstrapLeaderboards();
+        }
+
+        public void Play(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+        public void Quit()
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+
+
+
+        private void BootstrapLeaderboards()
+        {
+            foreach (var leaderboard in _leaderboards)
+            {
+                SaveManager.LoadLeaderboard(leaderboard.name);
+            }
+        }
+    }
+}
