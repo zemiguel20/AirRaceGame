17a7904d8779dfdf0e5cfc1eb80b2350160b9bc8
zemiguel20 <migli.coutinho@gmail.com>
2021-06-15 20:55:38 +0100

    #22 PauseMenu now reacts to Game Manager events

---
+++ Assets/Scripts/UI/Race/PauseMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using AirRace.Race;
+using UnityEngine;
+
+namespace AirRace.UI
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [SerializeField] private GameManager _gameManager;
+
+        private void Start()
+        {
+            gameObject.SetActive(false);
+
+            _gameManager.GamePaused += OnPause;
+            _gameManager.GameResumed += OnResume;
+        }
+
+        private void OnDestroy()
+        {
+            _gameManager.GamePaused -= OnPause;
+            _gameManager.GameResumed -= OnResume;
+        }
+
+        private void OnPause()
+        {
+            gameObject.SetActive(true);
+        }
+
+        private void OnResume()
+        {
+            gameObject.SetActive(false);
+        }
+
+
+    }
+}
