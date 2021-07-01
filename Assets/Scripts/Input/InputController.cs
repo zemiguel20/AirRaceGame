0f2a033320cea97559ef7074299711533dc42f94
zemiguel20 <migli.coutinho@gmail.com>
2021-07-01 19:53:39 +0100

    Namespace added to InputController

--- Assets/Scripts/Input/InputController.cs
+++ Assets/Scripts/Input/InputController.cs
@@ -1,52 +1,55 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputController : MonoBehaviour
+namespace AirRace.Input
 {
-    [SerializeField] [Range(0, 1)] private float _rotationInputSensivity;
-    [SerializeField] [Range(0, 1)] private float _accelerateInputSensivity;
-
-    public delegate void AccelerateHandler(float value);
-    public event AccelerateHandler AccelerateInputTriggered;
-
-    public delegate void AileronsHandler(float value);
-    public event AileronsHandler AileronsInputTriggered;
-
-    public delegate void ElevatorsHandler(float value);
-    public event ElevatorsHandler ElevatorsInputTriggered;
-
-    public delegate void RudderHandler(float value);
-    public event RudderHandler RudderInputTriggered;
-
-    public delegate void PauseHandler();
-    public event PauseHandler PauseInputTriggered;
-
-
-    public void OnAccelerate(InputAction.CallbackContext context)
+    public class InputController : MonoBehaviour
     {
-        AccelerateInputTriggered?.Invoke(context.ReadValue<float>() * _accelerateInputSensivity);
-    }
+        [SerializeField] [Range(0, 1)] private float _rotationInputSensivity;
+        [SerializeField] [Range(0, 1)] private float _accelerateInputSensivity;
 
-    public void OnAileronsMove(InputAction.CallbackContext context)
-    {
-        AileronsInputTriggered?.Invoke(context.ReadValue<float>() * _rotationInputSensivity);
-    }
+        public delegate void AccelerateHandler(float value);
+        public event AccelerateHandler AccelerateInputTriggered;
 
-    public void OnElevatorsMove(InputAction.CallbackContext context)
-    {
-        ElevatorsInputTriggered?.Invoke(context.ReadValue<float>() * _rotationInputSensivity);
-    }
+        public delegate void AileronsHandler(float value);
+        public event AileronsHandler AileronsInputTriggered;
 
-    public void OnRudderMove(InputAction.CallbackContext context)
-    {
-        RudderInputTriggered?.Invoke(context.ReadValue<float>() * _rotationInputSensivity);
-    }
+        public delegate void ElevatorsHandler(float value);
+        public event ElevatorsHandler ElevatorsInputTriggered;
 
-    public void OnPauseUnpauseGame(InputAction.CallbackContext context)
-    {
-        if (context.started)
+        public delegate void RudderHandler(float value);
+        public event RudderHandler RudderInputTriggered;
+
+        public delegate void PauseHandler();
+        public event PauseHandler PauseInputTriggered;
+
+
+        public void OnAccelerate(InputAction.CallbackContext context)
         {
-            PauseInputTriggered?.Invoke();
+            AccelerateInputTriggered?.Invoke(context.ReadValue<float>() * _accelerateInputSensivity);
+        }
+
+        public void OnAileronsMove(InputAction.CallbackContext context)
+        {
+            AileronsInputTriggered?.Invoke(context.ReadValue<float>() * _rotationInputSensivity);
+        }
+
+        public void OnElevatorsMove(InputAction.CallbackContext context)
+        {
+            ElevatorsInputTriggered?.Invoke(context.ReadValue<float>() * _rotationInputSensivity);
+        }
+
+        public void OnRudderMove(InputAction.CallbackContext context)
+        {
+            RudderInputTriggered?.Invoke(context.ReadValue<float>() * _rotationInputSensivity);
+        }
+
+        public void OnPauseUnpauseGame(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                PauseInputTriggered?.Invoke();
+            }
         }
     }
 }
