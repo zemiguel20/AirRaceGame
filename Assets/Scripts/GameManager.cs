62769a41ea961afc95b07e2393da02bc4cb5fe0d
zemiguel20 <migli.coutinho@gmail.com>
2021-07-15 14:38:59 +0100

    #32 GameManager - added singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using AirRace.Utils;
 using UnityEngine;
 
 namespace AirRace
 {
     public class GameManager : MonoBehaviour
     {
+        #region Singleton
+        public static GameManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            // Keeping one instance only
+            if (Instance != null && Instance != this)
+            {
+                GameLogger.Debug("GameManager already exists, removing this object.");
+                Destroy(GetComponent<GameManager>());
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+        #endregion
+
         // Start is called before the first frame update
         void Start()
         {
-        
+
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+
         }
     }
 }
