4717d2d8dc852ad39136b9b3d9b465d92511abc0
zemiguel20 <migli.coutinho@gmail.com>
2021-07-25 03:02:43 +0100

    #34 Chronometer refactored to reusable utility class

---
+++ Assets/Scripts/Utils/Chronometer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AirRace.Utils
+{
+    public class Chronometer
+    {
+        private float _timeCount;
+
+        public Chronometer()
+        {
+            _timeCount = 0;
+        }
+
+        public float Time { get => _timeCount; }
+
+
+        /// <summary> Note: added time is equal to the *absolute* value of given time. </summary>
+        public void Tick(float time)
+        {
+            _timeCount += Math.Abs(time);
+        }
+
+    }
+}
